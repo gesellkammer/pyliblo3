3a8604632de46acc64c350dde9d4b68b9e485ac2
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-01-25 23:57:27 +0100

    no python 2

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -6,10 +6,11 @@ set -e -x
 yum install -y liblo-dev
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+#for PYBIN in /opt/python/*/bin; do
     # "${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-done
+#    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+#done
+pip3 wheel /ui/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
@@ -17,7 +18,8 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-done
+#for PYBIN in /opt/python/*/bin/; do
+#    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+#done
+pip3 install python-manylinux-demo --no-index -f /io/wheelhoise
